ffc142d52cf764096ca03f90df2cef74de851a42
cwainwright <cwainwright@IE-016623>
2021-06-16 16:14:08 +0100

    Add project files.

---
+++ HitAndMiss/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace HitAndMiss
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click a colour";
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(29, 72);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(76, 53);
+            this.btnRed.TabIndex = 2;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.Location = new System.Drawing.Point(147, 128);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(76, 53);
+            this.btnBlack.TabIndex = 3;
+            this.btnBlack.Text = "Black";
+            this.btnBlack.UseVisualStyleBackColor = true;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(65, 128);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(76, 53);
+            this.btnYellow.TabIndex = 4;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(180, 72);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(76, 53);
+            this.btnGreen.TabIndex = 5;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(104, 72);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(76, 53);
+            this.btnBlue.TabIndex = 6;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Font = new System.Drawing.Font("Showcard Gothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnStartGame.Location = new System.Drawing.Point(12, 19);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(776, 419);
+            this.btnStartGame.TabIndex = 7;
+            this.btnStartGame.Text = "START GAME";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnBlue;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnStartGame;
+    }
+}
+
---
+++ HitAndMiss/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HitAndMiss
+{
+    public partial class Form1 : Form
+    {
+        int circle1X = 395;
+        int CircleyAxis = 70;
+        int circle2X = 455;
+        int circle3X = 515;
+        int circleNumber = 1;
+        int circleLine = 0;
+        int[] circleArray = { 70, 120, 170, 220, 270, 320 };
+        string[] guess = new string[3];
+        string[] answer = { "Red", "Green", "Yellow", "Blue", "Black" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Randomizer
+        {
+            public static void Randomize<T>(T[] items)
+            {
+                Random rand = new Random();
+
+                // For each spot in the array, pick
+                // a random item to swap into that spot.
+                for (int i = 0; i < items.Length - 1; i++)
+                {
+                    int j = rand.Next(i, items.Length);
+                    T temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+        private void DrawCircleOutline(int xAxis, int yAxis)
+        {
+            Graphics myGraphics = base.CreateGraphics();
+            Pen myPen = new Pen(Color.Black);
+            SolidBrush mySolidBrush = new SolidBrush(Color.Red);
+            myGraphics.DrawEllipse(myPen, xAxis, yAxis, 40, 40);
+        }
+        private void DrawCircle(int xAxis, int yAxis, Brush colour)
+        {
+            Graphics myGraphics = base.CreateGraphics();
+            Pen myPen = new Pen(Color.Black);
+            SolidBrush mySolidBrush = new SolidBrush(Color.Black);
+            myGraphics.DrawEllipse(myPen, xAxis, yAxis, 40, 40);
+            myGraphics.FillEllipse(colour, xAxis, yAxis, 40, 40);
+        }
+        private void ButtonPush(Brush colour, string colourGuess)
+        {
+            if (circleLine < circleArray.Length)
+            {
+                switch (circleNumber)
+                {
+                    case 1:
+                        DrawCircle(circle1X, circleArray[circleLine], colour);
+                        guess[0] = colourGuess;
+                        circleNumber = 2;
+                        break;
+                    case 2:
+                        DrawCircle(circle2X, circleArray[circleLine], colour);
+                        guess[1] = colourGuess;
+                        circleNumber = 3;
+                        break;
+                    case 3:
+                        DrawCircle(circle3X, circleArray[circleLine], colour);
+                        guess[2] = colourGuess;
+                        circleNumber = 1;
+                        circleLine += 1;
+                        if (guess.SequenceEqual(answer))
+                        {
+                            MessageBox.Show("Congrate on guessing the correct sequence", "Correct", MessageBoxButtons.OK);
+                        } else if (circleLine < circleArray.Length)
+                        {
+                            DrawCircleOutline(circle1X, circleArray[circleLine]);
+                            DrawCircleOutline(circle2X, circleArray[circleLine]);
+                            DrawCircleOutline(circle3X, circleArray[circleLine]);
+                        } else if (circleLine == circleArray.Length)
+                        {
+                            int newXAxis = 395;
+                            for (int i = 0; i < answer.Length; i++)
+                            {
+                                switch (answer[i])
+                                {
+                                    case "Red":
+                                        DrawCircle(newXAxis, 370, Brushes.Red);
+                                        break;
+                                    case "Yellow":
+                                        DrawCircle(newXAxis, 370, Brushes.Yellow);
+                                        break;
+                                    case "Blue":
+                                        DrawCircle(newXAxis, 370, Brushes.Blue);
+                                        break;
+                                    case "Black":
+                                        DrawCircle(newXAxis, 370, Brushes.Black);
+                                        break;
+                                    case "Green":
+                                        DrawCircle(newXAxis, 370, Brushes.Green);
+                                        break;
+                                }
+                                newXAxis += 60;
+                            }
+                        }
+                        break;
+                }
+            } 
+        }
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            ButtonPush(Brushes.Red, "Red");
+        }
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            ButtonPush(Brushes.Blue, "Blue");
+        }
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            ButtonPush(Brushes.Green, "Green");
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            ButtonPush(Brushes.Yellow, "Yellow");
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            ButtonPush(Brushes.Black, "Black");
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            btnStartGame.Dispose();
+            DrawCircleOutline(circle1X, CircleyAxis);
+            DrawCircleOutline(circle2X, CircleyAxis);
+            DrawCircleOutline(circle3X, CircleyAxis);
+            DrawCircle(circle1X, CircleyAxis + 300, Brushes.White);
+            DrawCircle(circle2X, CircleyAxis + 300, Brushes.White);
+            DrawCircle(circle3X, CircleyAxis + 300, Brushes.White);
+            Randomizer.Randomize(answer);
+            Array.Resize(ref answer, 3);
+        }
+    }
+}
